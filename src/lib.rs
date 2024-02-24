bd4cb678b8fce09a3ef74b924c1a7ba33be0f70b
Peter Bjorklund <piot@hotmail.com>
2024-02-24 20:30:54 +0100

    chore: rename

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------------------------------------
- *  Copyright (c) Peter Bjorklund. All rights reserved. https://github.com/piot/substrata-rs
+ *  Copyright (c) Peter Bjorklund. All rights reserved. https://github.com/piot/fixed32-math-rs
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------------------*/
 use std::ops::{Add, AddAssign, Div, Mul, Sub};
